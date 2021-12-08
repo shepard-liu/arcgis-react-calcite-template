9ed3bb8ac1d85491b2114a0205c4ec721c20ff22
shepard_liu <1057927664@qq.com>
2021-12-08 08:42:25 +0800

    replaced npm script "copy -assets" with "devBuildWithAssets"

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,26 +2,48 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const env = {
-    production: false,
-}
+module.exports = (env) => {
+    // post build configuration
+    const useCopyPlugin = env.copy !== undefined;
 
-module.exports = merge(common(env), {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    // A development server for serving up our files
-    devServer: {
-        static: path.join(__dirname, 'dist'),
-        port: 3000,
-        host: 'localhost',
-        // With this enabled, page refreshes automatically when rebuilt
-        hot: true,
-        // Need to copy assets to dist, so force the dev server to write to disk
-        devMiddleware: {
-            index: true,
-            publicPath: '/',
-            writeToDisk: true,
+    const plugins = [];
+
+    if (useCopyPlugin) {
+        // Copy assets from @arcgis and @esri to dist/assets
+        plugins.push(new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.join(__dirname, 'node_modules/@arcgis/core/assets'),
+                    to: path.join(__dirname, 'dist/assets')
+                },
+            ]
+        }));
+    }
+
+    // define a mode flag for common configuration
+    const envForCommon = {
+        production: false,
+    }
+
+    return merge(common(envForCommon), {
+        mode: 'development',
+        devtool: 'inline-source-map',
+        // A development server for serving up our files
+        devServer: {
+            static: path.join(__dirname, 'dist'),
+            port: 3000,
+            host: 'localhost',
+            // With this enabled, page refreshes automatically when rebuilt
+            hot: true,
+            // Need to copy assets to dist, so force the dev server to write to disk
+            devMiddleware: {
+                index: true,
+                publicPath: '/',
+                writeToDisk: true,
+            },
         },
-    },
-});
+        plugins
+    });
+}
--- webpack.prod.js
+++ webpack.prod.js
@@ -4,25 +4,29 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const env = {
-    production: true,
-}
 
-module.exports = merge(common(env), {
-    mode: 'production',
-    devtool: 'source-map',
-    output: {
-        clean: true,
-    },
-    plugins: [
-        // Copy assets from @arcgis and @esri to dist/assets
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.join(__dirname, 'node_modules/@arcgis/core/assets'),
-                    to: path.join(__dirname, 'dist/assets')
-                },
-            ]
-        }),
-    ]
-});
+module.exports = (env) => {
+
+    const envForCommon = {
+        production: true,
+    }
+
+    return merge(common(envForCommon), {
+        mode: 'production',
+        devtool: 'source-map',
+        output: {
+            clean: true,
+        },
+        plugins: [
+            // Copy assets from @arcgis and @esri to dist/assets
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: path.join(__dirname, 'node_modules/@arcgis/core/assets'),
+                        to: path.join(__dirname, 'dist/assets')
+                    },
+                ]
+            }),
+        ]
+    });
+}
